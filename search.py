923bb71b88d405b0164679703f2dfd4311a7319e
satomi-ueda21 <gionkuri@hotmail.com>
2021-12-16 16:24:56 +0900

    task3-4 ログが表示されない

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ import pandas as pd
 import eel
 
 ### デスクトップアプリ作成課題
+@eel.expose
 def kimetsu_search(word):
     # 検索対象取得
     df=pd.read_csv("./source.csv")
@@ -9,15 +10,17 @@ def kimetsu_search(word):
 
     # 検索
     if word in source:
-        print("『{}』はあります".format(word))
+        log_word = "『{}』はあります".format(word)
     else:
-        print("『{}』はありません".format(word))
+        log_word = "『{}』はありません".format(word)
         # 追加
         #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
         #if add_flg=="1":
         source.append(word)
-    
+
     # CSV書き込み
     df=pd.DataFrame(source,columns=["name"])
     df.to_csv("./source.csv",encoding="utf_8-sig")
     print(source)
+    return log_word
+
