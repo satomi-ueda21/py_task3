fad651d02a3b0a44bcd1890d3de25c07cf2f9574
satomi-ueda21 <gionkuri@hotmail.com>
2021-12-17 12:54:42 +0900

    task3-4 ログが表示されないまま

--- search.py
+++ search.py
@@ -2,7 +2,6 @@ import pandas as pd
 import eel
 
 ### デスクトップアプリ作成課題
-@eel.expose
 def kimetsu_search(word):
     # 検索対象取得
     df=pd.read_csv("./source.csv")
@@ -10,9 +9,10 @@ def kimetsu_search(word):
 
     # 検索
     if word in source:
-        log_word = "『{}』はあります".format(word)
+        print("『{}』はいます".format(word))
+        eel.view_log_js("『{}』はいます".format(word))
     else:
-        log_word = "『{}』はありません".format(word)
+        eel.view_log_js("『{}』はいません".format(word))
         # 追加
         #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
         #if add_flg=="1":
@@ -22,5 +22,5 @@ def kimetsu_search(word):
     df=pd.DataFrame(source,columns=["name"])
     df.to_csv("./source.csv",encoding="utf_8-sig")
     print(source)
-    return log_word
+
 
--- view.py
+++ view.py
@@ -9,6 +9,6 @@ size=(700,600)
 @ eel.expose
 def kimetsu_search(word):
     search.kimetsu_search(word)
-    
+
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
