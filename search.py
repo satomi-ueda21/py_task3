e497b8a47a4e36a933e5537186d3102e4bc89df9
satomi-ueda21 <gionkuri@hotmail.com>
2021-12-19 10:16:09 +0900

    task3-6完了

--- search.py
+++ search.py
@@ -5,24 +5,33 @@ import os
 ### デスクトップアプリ作成課題
 def kimetsu_search(word, file_path, file_name):
     # 検索対象取得
-    new_file_path = os.path.join(file_path, file_name) + ".csv"
-    new_file = str(file_name + ".csv")
-    #ファイルの保存場所と名前を指定する。何も指定がなければ既存のcsv読み込み。
+    new_file_path = str(os.path.join(file_path, file_name) + ".csv")
+    new_dir = str(os.path.join(file_path, 'source.csv'))
+    new_file =str("./" + file_name + ".csv")
+    s = ",name"
+    #新しいファイル保存場所とファイル名指定。
     if file_path != "" and file_name != "":
         #ディレクトリが存在しない場合作成
         os.makedirs(file_path, exist_ok=True)
         #ファイルが存在しない場合作成
         if not os.path.exists(new_file_path):
-            with open(new_file_path, 'w'):
-                pass
+            with open(new_file_path, 'w') as f:
+                f.write(s)
         df=pd.read_csv(new_file_path)
+    #新しい保存場所のみ指定。
     elif file_path != "" and file_name == "":
-        if not os.path.exists(new_file):
-            with open(new_file, "w"):
-                pass
-        df=pd.read_csv("./{}".format(new_file))
+        os.makedirs(file_path, exist_ok=True)
+        if not os.path.exists(new_dir):
+            with open(new_dir, 'w') as f:
+                f.write(s)
+        df=pd.read_csv(new_dir)
+    #新しいファイル名のみ指定。
     elif file_path == "" and file_name != "":
-
+        if not os.path.exists(new_file):
+            with open(new_file, "w") as f:
+                f.write(s)
+        df=pd.read_csv(new_file)
+    #既存のCSVファイルを使用。
     else:
         df=pd.read_csv("./source.csv")
 
@@ -48,9 +57,9 @@ def kimetsu_search(word, file_path, file_name):
     if file_path != "" and file_name != "":
         df.to_csv(new_file_path, encoding="utf_8-sig")
     elif file_path != "" and file_name == "":
-        df.to_csv("./{}".format(new_file), encoding="utf_8-sig")
+        df.to_csv(new_dir, encoding="utf_8-sig")
     elif file_path == "" and file_name != "":
-        df.to_csv("{}/source.csv".format(file_path),encoding="utf_8-sig")
+        df.to_csv(new_file, encoding="utf_8-sig")
     else:
         df.to_csv("./source.csv", encoding="utf_8-sig")
     print(source)
